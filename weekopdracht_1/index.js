9c95e001c602f83fc2ac9f62eef8f28fbb773b5e
lars <>
2020-08-31 03:34:17 -0700

    weekopdracht1 tot local storage

--- weekopdracht_1/index.js
+++
@@ -1,8 +0,0 @@
-function click_cell() {
-    let rows = document.getElementById('4by4_table').rows;
-    for (let i = 0; i < rows.clientHeight; i++) {
-        for (let j = 0; j < rows[i].cells.length; j++) {
-            rows[i].cells[j].addEventListener('click', console.log('click'));
-        }
-    }
-}
